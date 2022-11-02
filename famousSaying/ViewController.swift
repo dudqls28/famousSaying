29bc3389ce08d14f8cf5863ef0edef91a30576d9
yblim <dudqls28@naver.com>
2022-11-02 10:53:53 +0900

    Initial Commit

---
+++ famousSaying/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  famousSaying
+//
+//  Created by hwict on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
